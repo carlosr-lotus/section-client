fde71f0052846ac0f0f1f07c1a07d22e245edb83
Carlos Lotus <carlosr_juniorpro@protonmail.com>
2024-10-16 17:33:56 -0300

    UPD input field component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,12 @@
+import InputField from "./material/InputField/InputField";
+
 import styles from "./Header.module.css";
 
 export default function Header(): JSX.Element {
-    return (
-        <header className={styles.header}>
-            <div className={styles.logo}>/s</div>
-            <input type="text" placeholder="Search..." className={styles.search}/>
-        </header>
-    )
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>/s</div>
+      <InputField name="Search" placeholder="Search..." type="text" />
+    </header>
+  );
 }
